793e7f4ba1c05091a24f162b73d59c756d8127c3
brad-decker <bhdecker84@gmail.com>
2015-08-10 16:40:40 -0500

    added first test, updated travis.yml

---
+++ test/basic.js
@@ -0,0 +1,28 @@
+import should from 'should';
+import Model from './../src/model.js';
+import mongoose from 'mongoose';
+
+let instance;
+
+/**
+ * This is a simple test class to test some of the most basic features of the module
+ * @class Simple
+ * @augments mongoose.Model
+ * @inheritdoc mongoose.Model
+ */
+class Simple extends Model {
+  name = 'STRING';
+  type = 'STRING';
+}
+
+
+describe( 'Basic Functionality', () => {
+  it( 'should allow an instance to be created', () => {
+    instance = new Simple();
+  } );
+
+  it( 'should have two fields', () => {
+    instance.generateOptions();
+    instance._fields.should.have.property( 'name' );
+  } );
+} );
