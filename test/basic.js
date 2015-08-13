14092adf351df448eb89fcc3105543e26902a4b7
brad-decker <bhdecker84@gmail.com>
2015-08-13 12:34:53 -0500

    new tests :D

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import Model from './../src/model.js';
+import Model from './../src/model';
+import { validate, hook, index, beforeCreate } from './../src/decorators';
 
 let instance;
 
@@ -12,6 +13,9 @@ let instance;
 class Simple extends Model {
   name = 'STRING';
   type = 'STRING';
+
+  @index()
+  uniqueName = { unique: true, fields: ['email'] };
 
   static staticTest () {
     console.log( 'static' );
@@ -28,10 +32,30 @@ class Simple extends Model {
   set cool ( value ) {
     this.type = value;
   }
+
+  get _name ( ) {
+    return this.name;
+  }
+
+  set _name ( value ) {
+    this.name = value;
+  }
+
+  @validate()
+  nameCantBeType () {
+    if ( this.name === this.type ) {
+      throw new Error( 'Name cannot be the same as type' );
+    }
+  }
+
+  @beforeCreate()
+  static nameToLower ( simple ) {
+    simple.name = simple.name.toLowerCase();
+  }
 }
 
 
-describe( 'Basic Functionality', () => {
+describe( 'Model instances', () => {
 
   it( 'should allow an instance to be created', () => {
     instance = new Simple();
@@ -44,16 +68,19 @@ describe( 'Basic Functionality', () => {
   } );
 
   it( 'should collect field declarations into the _fields object', () => {
-    expect( instance._fields ).to.have.property( 'name' ).that.is.a( 'string' );
+    expect( instance._fields ).to.have.property( 'name' ).that.is.a( 'object' );
     expect( instance._fields ).to.have.property( 'type' ).that.is.a( 'string' );
+    expect( instance ).to.not.have.property( 'uniqueEmail' );
   } );
 
   it( 'should collect static functions into the _classMethods object', () => {
     expect( instance._classMethods ).to.have.property( 'staticTest' ).that.is.a( 'function' );
+    expect( instance._instanceMethods ).to.not.have.property( 'nameToLower' );
   } );
 
   it( 'should collect member methods into the _instanceMethods object', () => {
     expect( instance._instanceMethods ).to.have.property( 'test' ).that.is.a( 'function' );
+    expect( instance._instanceMethods ).to.not.have.property( 'nameCantBeType' );
   } );
 
   it( 'should collect getter methods into the _getterMethods object', () => {
@@ -64,5 +91,29 @@ describe( 'Basic Functionality', () => {
     expect( instance._setterMethods ).to.have.property( 'cool' ).that.is.a( 'function' );
   } );
 
+  it( 'should modify the declaration of fields with getters', () => {
+    expect( instance._fields.name ).to.have.property( 'get' ).that.is.a( 'function' );
+  } );
+
+  it( 'should modify the declaration of fields with setters', () => {
+    expect( instance._fields.name ).to.have.property( 'set' ).that.is.a( 'function' );
+  } );
+
+  it( 'should have a _indexes property', () => {
+    expect( instance.constructor ).to.have.property( '_indexes' );
+    expect( instance.constructor._indexes ).to.have.length( 1 );
+  } );
+
+  it( 'should have a _validate property', () => {
+    expect( instance.constructor ).to.have.property( '_validate' ).that.is.a( 'object' );
+    expect( instance.constructor._validate ).to.have.property( 'nameCantBeType' ).that.is.a( 'function' );
+  } );
+
+  it( 'should have a _hooks property', () => {
+    expect( instance.constructor ).to.have.property( '_hooks' ).that.is.a( 'object' );
+    expect( instance.constructor._hooks ).to.have.property( 'nameToLower' ).that.is.a( 'object' );
+    expect( instance.constructor._hooks.nameToLower ).to.have.property( 'fn' ).that.is.a( 'function' );
+    expect( instance.constructor._hooks.nameToLower ).to.have.property( 'action' ).that.is.a( 'string' );
+  } );
 
 } );
