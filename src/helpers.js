4a3ba89852ebe304d40ff55035db78527c76084d
brad-decker <bhdecker84@gmail.com>
2015-08-13 12:33:37 -0500

    refactored using generators

--- src/helpers.js
+++ src/helpers.js
@@ -1,17 +1,13 @@
 
-const ignore = {
-  methods: [
-    'constructor',
-    'Model'
-  ],
-  statics: [
-    'length',
-    'name',
-    'arguments',
-    'caller',
-    'prototype'
-  ]
-};
+const ignore = [
+  'constructor',
+  'Model',
+  'length',
+  'name',
+  'arguments',
+  'caller',
+  'prototype'
+];
 
 export function* entries ( obj ) {
   for ( let key of Object.keys( obj ) ) {
@@ -30,71 +26,12 @@ export function getProperties ( model ) {
   return properties;
 }
 
-export function getMemberFunctions ( model ) {
+export function defineFunctions ( model ) {
 
-  let prototype = Object.getPrototypeOf( model );
-
-  // Get the property names on the prototype
-  let functions = Object.getOwnPropertyNames( prototype );
-
-  // Start a list of methods to return
-  let methods = {};
-
-  // Loop through method name strings
-  for ( let [ methodName, method ] of findFunctions( functions, prototype ) ) {
-    methods[ methodName ] = method;
+  for ( let [ target, name, method ] of findFunctions( model ) ) {
+    model[target][name] = method;
   }
 
-  // Return the methods.
-  return methods;
-}
-
-export function getFunctionsOfType ( model, type = 'get' ) {
-
-  let prototype = Object.getPrototypeOf( model );
-
-  // Get the property names on the prototype
-  let functions = Object.getOwnPropertyNames( prototype );
-
-  // Start a list of getters to return
-  let foundFunctions = {};
-
-  // Loop through method name strings
-  for ( let [ methodName, method ] of findFunctions( functions, prototype, type ) ) {
-
-    if ( isField( methodName, model ) ) {
-
-      let field = fieldName( methodName );
-      model._fields[ field ] = addToDefinition( model._fields[ field ], method, type );
-      continue;
-
-    }
-
-    foundFunctions[ methodName ] = method;
-  }
-
-  // Return the getters.
-  return foundFunctions;
-}
-
-export function getStaticFunctions ( model ) {
-
-  // Get the Object constructor to get static functions
-  let constructor = model.constructor;
-
-  // Get the property names on the constructor
-  let functions = Object.getOwnPropertyNames( constructor );
-
-  // Start a object of statics to return
-  let statics = {};
-
-  // Loop through method name strings
-  for ( let [ methodName, method ] of findFunctions( functions, constructor, 'static' ) ) {
-    statics[ methodName ] = method;
-  }
-
-  // Return the statics.
-  return statics;
 }
 
 /** helpers **/
@@ -103,48 +40,85 @@ function addToDefinition ( field, method, type = 'get' ) {
   if ( typeof field !== 'object' ) {
     field = { type: field };
   }
-  field[ type ] = method;
-  return true;
+
+  if ( method.get ) field.get = method.get;
+  if ( method.set ) field.set = method.set;
+
+  return field;
 }
 
-function* findFunctions ( names, object, flag = 'methods' ) {
+function* findFunctions ( model ) {
 
-  let filters = ignore.methods;
-  let filterFunc = method => {
-    return ( typeof method.value !== 'function' || typeof method.get === 'function' || typeof method.set === 'function' );
-  };
-  let returnValue = method => method.value;
-  let checkWritable = method => !method.writable;
+  let prototype = Object.getPrototypeOf( model );
+  let constructor = model.constructor;
 
-  switch ( flag ) {
-    case 'get':
-      filterFunc = method => typeof method.get !== 'function';
-      returnValue = method => method.get;
-      checkWritable = method => false;
-      break;
-    case 'set':
-      filterFunc = method => typeof method.set !== 'function';
-      returnValue = method => method.set;
-      break;
-    case 'static':
-      filterFunc = method => typeof method.value !== 'function';
-      filters = ignore.statics;
-      break;
-    default:
-      break;
+  let staticFunctions = getFunctions( constructor, true );
+  let memberFunctions = getFunctions( prototype );
+
+  for ( let method of staticFunctions.filter( filterFunction ) ) {
+    for ( let result of parseFunction( method, model ) ) {
+      yield result;
+    }
   }
 
-  for ( let name of names ) {
+  for ( let method of memberFunctions.filter( filterFunction ) ) {
+    for ( let result of parseFunction( method, model ) ) {
+      yield result;
+    }
+  }
+}
 
-    let method = Object.getOwnPropertyDescriptor( object, name );
+function* parseFunction ( method, model ) {
 
-    if ( filters.indexOf( name ) >= 0 || filterFunc( method ) || checkWritable( method ) ) {
-      continue;
+  if ( method.get || method.set ) {
+
+    if ( !isField( method.name, model ) ) {
+
+      if ( method.get ) yield [ '_getterMethods', method.name, method.get ];
+      if ( method.set ) yield [ '_setterMethods', method.name, method.set ];
+
     }
 
-    yield [ name, returnValue( method ) ];
+    let field = fieldName( method.name );
+    model._fields[ field ] = addToDefinition( model._fields[ field ], method );
+
   }
 
+  let target = '_instanceMethods';
+
+  if ( method.isStatic === true ) {
+    target = '_classMethods';
+  }
+
+  yield [ target, method.name, method.value ];
+}
+
+function filterFunction ( method ) {
+
+  if ( ignore.indexOf( method.name ) >= 0 ) {
+    return false;
+  }
+
+  if ( method.writable && typeof method.value === 'function' ) {
+    return true;
+  }
+
+  return ( ( method.get && typeof method.get === 'function' ) || ( method.set && typeof method.set === 'function' ) );
+
+}
+
+function getFunctions ( object, isStatic = false ) {
+  let names = Object.getOwnPropertyNames( object );
+  let funcs = [];
+
+  for ( let name of names ) {
+    let descriptor = Object.getOwnPropertyDescriptor( object, name );
+    descriptor.name = name;
+    descriptor.isStatic = isStatic;
+    funcs.push( descriptor );
+  }
+
+  return funcs;
 }
 
 function fieldName ( name ) {
