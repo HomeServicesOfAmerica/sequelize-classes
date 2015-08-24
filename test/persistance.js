5fda18d91b5503181a5e3fc370e445d3025c40b3
brad-decker <bhdecker84@gmail.com>
2015-08-24 10:12:40 -0500

    Connected to database in test, preparing to create persistance testing.

---
+++ test/persistance.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+import { expect } from 'chai';
+import dotenv from 'dotenv';
+
+dotenv.load();
+
+let sequelize;
+
+before( () => {
+  sequelize = new Sequelize( process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+    port: process.env.PORT,
+    dialect: 'postgres'
+  } );
+} );
+
+describe( 'sequelize instance', () => {
+
+  it( 'should be connected and authenticated', done => {
+    sequelize.authenticate().then( done ).catch( err => { throw err; } );
+  } );
+
+} );
